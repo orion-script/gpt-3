ce8a1e97821f3ea6e32e3a5ac0bf89e8cf80068f
Olaniyi John Ayomide <jaolaniyi96@student.lautech.edu.ng>
2023-01-31 22:10:43 +0100

    add

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
     return (
         <nav className="flex justify-between w-10/12 m-auto py-10">
             <img src={Logo} alt=""/>
-            <img src={isMenuOpen ? Menu : Close} alt="Menu" className="md:hidden" onClick={toggleSideBar}/>
+            <img src={Menu} alt="Menu" className="md:hidden" onClick={toggleSideBar}/>
             <div className="hidden md:flex justify-between w-10/12">
                 <div className="flex justify-between w-2/4">
                     <a href="#home">Home</a>
@@ -34,14 +34,15 @@ const NavBar = () => {
                 </div>
             </div>
 
-            <div ref={openRef} className="hidden toggleOpen absolute bg-gray-800 w-3/4 h-screen py-10 md:hidden right-0 top-20 font-bold text-xl flex flex-col rounded-l-lg">
-                <ul className="w-3/4 pl-10">
+            <div ref={openRef} className="hidden toggleOpen absolute bg-gray-800 w-3/4 h-screen py-10 md:hidden right-0 top-0 font-bold text-xl flex flex-col rounded-l-lg">
+                <img src={Close} alt="Menu" className="w-1/12 absolute right-5" onClick={toggleSideBar}/>
+                <div className="w-3/4 pl-10 pt-10 flex flex-col">
                     <a href="#home" className="py-5">Home</a>
-                    <li className="py-5">What is GPT?</li>
-                    <li className="py-5">Open AI</li>
-                    <li className="py-5">Case Studies</li>
-                    <li className="py-5">Library</li>
-                </ul>
+                    <a href="#whatisgpt" className="py-5">What is GPT?</a>
+                    <a href="#openai" className="py-5">Open AI</a>
+                    <a href="#casestudie" className="py-5">Case Studies</a>
+                    <a href="#library" className="py-5">Library</a>
+                </div>
                 <div className="flex flex-col w-2/4 m-auto">
                     <button className="pb-10">Sign In</button>
                     <button className="bg-buttonBackground rounded">
